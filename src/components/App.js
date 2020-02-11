269b8cdf9d5312e63088059d298fbe7bf97e46c0
Danny Aziz <daanyaal997@gmail.com>
2020-02-11 14:51:49 +0000

    INIT

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+            </Switch>
+        </Router>
+    );
+}
+
+export default App;
