d663a052e4cc08356a87a5eded48d4132115af13
Danny Aziz <daanyaal997@gmail.com>
2020-02-11 14:55:39 +0000

    Basic routes setup

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,23 @@ import React from 'react';
 import {
     BrowserRouter as Router,
     Switch,
-    Route,
-    Link
+    Route
 } from "react-router-dom";
+
+import Home from '../routes/Home';
+import Profile from '../routes/Profile';
 
 
 function App() {
     return (
         <Router>
             <Switch>
+                <Route path='/profile'>
+                    <Profile />
+                </Route>
+                <Route path='/'>
+                    <Home />
+                </Route>
             </Switch>
         </Router>
     );
