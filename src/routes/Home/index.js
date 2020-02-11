b8818c6fd4b665df73b1242c9953be5f97f59303
Danny Aziz <daanyaal997@gmail.com>
2020-02-11 17:36:01 +0000

    Cluster Selector

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './style.css';
+
+const CLUSTER_IMAGE_MAP = {
+    'Bitcoin': require('../../assets/BTC.png'),
+    'Ethereum': require('../../assets/ETH.png'),
+    'Ripple': require('../../assets/XRP.png'),
+    'All Crypto': require('../../assets/Crypto.png'),
+}
+
+function Cluster(props) {
+    const isActive = props.activeCluster === props.name;
+    const setActiveCluster = () => {
+        props.setActiveCluster(props.name);
+    }
+    return (
+        <div 
+            className={`cluster${(isActive ? ' cluster-active': '')}`}
+            onClick={setActiveCluster}
+        >
+            <img src={CLUSTER_IMAGE_MAP[props.name]} />
+            <span>{props.name}</span>
+        </div>
+    )
+}
 
 function Home() {
+    const [activeCluster, setActiveCluster] = useState('Bitcoin');
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={'container'}>
+            <section className={'cluster-selection'}>
+                <div className={'cluster-group'}>
+                    <Cluster name={'Bitcoin'} activeCluster={activeCluster} setActiveCluster={setActiveCluster} />
+                    <Cluster name={'Ethereum'} activeCluster={activeCluster} setActiveCluster={setActiveCluster} />
+                    <Cluster name={'Ripple'} activeCluster={activeCluster} setActiveCluster={setActiveCluster} />
+                    <Cluster name={'All Crypto'} activeCluster={activeCluster} setActiveCluster={setActiveCluster} />
+                </div>
+            </section>
+            <section>
+
+            </section>
+            <section>
+
+            </section>
         </div>
     )
 }
